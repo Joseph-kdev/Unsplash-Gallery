4fe93e57dc5913e35fe3a3e02fa29d37c4e822c1
Joseph Kinuthia <cjkinuthia5@gmail.com>
2023-09-20 12:46:58 +0300

    masonry-layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import { useState, useEffect, useRef} from 'react'
 import axios from 'axios'
+import Masonry from 'react-masonry-css'
 
 
 const SearchApp = () => {
@@ -8,10 +9,12 @@ const SearchApp = () => {
   const [results, setResults] = useState([])
   const ref = useRef('')
   const searchInput = useRef(null);
+  const [searchDone, setSearchDone] = useState(false)
 
   const search = (event) => {
     event.preventDefault()
     setSearchItem(ref.current)
+    setSearchDone(true)
   }
 
   const handleChange = (event) => {
@@ -31,27 +34,39 @@ const SearchApp = () => {
 
   }, [searchItem])
 
+const breakPoints = {
+  default: 3,
+  1100: 3,
+  700: 2,
+}
+
   return (
     <div>
       <form onSubmit={search} className='searchPart'>
         <input ref={searchInput} placeholder='Search images...' onChange={handleChange} className='searchBar'/>
       </form>
-        <div className='container'>
+      {searchDone && results.length === 0 ? (
+        <p className='notify'>No results for {searchItem}</p>
+      ) : (
+      <Masonry
+        breakpointCols={breakPoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
           {results.map(result => (
-          <img className='images' key={result.id} src={result.urls.small} alt={result.description} />
+          <div key={result.id}>
+          <img className='images' src={result.urls.small} alt={result.description} />
+          </div>
           ))}
-        </div>
+      </Masonry>
+      )}
     </div>
    );
 }
- 
 const App = () => {
-  
   return (
     <div className='app'>
       <SearchApp />
     </div>
    );
 }
- 
 export default App;
